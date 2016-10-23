ed3c32a8547b192757d2951711f5cb6610c5ba36
pranavsricharan <pranav.sricharan@gmail.com>
2016-10-23 15:42:09 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+require_once 'lib/potion.php';
+$data = array();
+$data['sitename'] = "Potion Brewer";
+$data['footer'] = "Created by: <a href='http://github.com/pranavsricharan' target='_blank'>Pranav Sricharan</a>";
+$template = new Potion("assets/templates/Sample/index.html",$data);
+
+$template->set('heading',"Templating without hassle");
+$template->set('coverData',"Brew your templates like magic");
+
+$template->set('sidebar',"Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.");
+if(isset($_GET['page']))
+{
+	$page = $_GET['page'];
+	$template->set('title',ucfirst($page));
+}
+else
+{
+	$page = "home";
+	$template->set('title',"Home");
+}
+if($page == "home")
+{
+	
+	$template->set('mainData',"Welcome to the sample site of potion brewer. Check the usage page on how to use this template engine.");
+}
+elseif($page == "usage")
+{
+	$template->set('mainData',file_get_contents("usage.html"));
+}
+elseif($page == "page1" || $page == "page2")
+{
+	$template->set('mainData',"<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p><br /><p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Nam liber tempor cum soluta nobis eleifend option congue nihil imperdiet doming id quod mazim placerat facer possim assum. Typi non habent claritatem insitam; est usus legentis in iis qui facit eorum claritatem. Investigationes demonstraverunt lectores legere me lius quod ii legunt saepius. Claritas est etiam processus dynamicus, qui sequitur mutationem consuetudium lectorum. Mirum est notare quam littera gothica, quam nunc putamus parum claram, anteposuerit litterarum formas humanitatis per seacula quarta decima et quinta decima. Eodem modo typi, qui nunc nobis videntur parum clari, fiant sollemnes in futurum.</p>");
+}
+else
+{
+	$template->set('mainData','');
+	$template->set('title','Page not found');
+	$template->set('heading','Error 404');
+	$template->set('coverData','Page not found');
+}
+$template->render();
+?>
---
+++ lib/potion.php
@@ -0,0 +1,76 @@
+<?php
+class Potion
+{
+	// Initialize the Potion brewing engine
+	public function __construct($path,$data=array())
+	{
+		$this->path = $path; // Path to template file
+		/*
+		**
+		** The data is passed as an associative array
+		** The data passed can be accessed in the template by using the index name
+		** For instance, the value at $data['title'] can be accessed in the template as {title} anywhere on the whole page
+		**
+		*/
+		$this->data = $data; // Data to be rendered on view
+	}
+	// Method to add values to the data array
+	public function set($key,$value)
+	{
+		$this->data[$key] = $value;
+	}
+	
+	// The main callable function to render the template
+	public function render()
+	{
+		// Check if template file exists
+		if(file_exists($this->path))
+		{
+			$this->fileData = file_get_contents($this->path); // Load the file for parsing if exists
+			$this->parseStatic(); // Parse the HTML file static data paths (like css,scripts and images)
+			$this->parseData(); // Parse the HTML file for brewing
+			$this->displayData(); // Display the final rendered result to the view
+		}
+		else
+		{
+			exit("<b>Error: </b>The following file does not exist<pre>" . $this->path . "</pre>"); // Terminate execution if file doesn't exist
+		}
+		
+	}
+	
+	// Parse static file paths
+	private function parseStatic()
+	{
+		// Find all the brewable portions of the template
+		preg_match_all("({static ([^\}]+)})",$this->fileData,$matches);
+		// Map all the potion static files to actual path
+		for($i=0;$i<sizeof($matches[1]);$i++)
+		{
+			$this->fileData = preg_replace("(" . $matches[0][$i] . ")","assets/static/".$matches[1][$i],$this->fileData);//Replace the potion variable with actual path to file
+		}
+	}
+	
+	// Parse the template file for variables
+	private function parseData()
+	{
+		// Find all the brewable portions of the template
+		preg_match_all("({[\w]+})",$this->fileData,$matches);
+		
+		// Replace all the potion variables with actual data
+		foreach($matches[0] as $match)
+		{
+			$replacementVar = trim($match,'{}'); // Trim the braces to find the array key
+			if(array_key_exists($replacementVar,$this->data)) //Find if key(potion variable) is set
+			{
+				$this->fileData = preg_replace("($match)",$this->data[$replacementVar],$this->fileData);//Replace the potion variable with actual page data
+			}
+		}
+	}
+	
+	// Display the brewed page
+	private function displayData()
+	{
+		echo $this->fileData;
+	}
+}
+?>
